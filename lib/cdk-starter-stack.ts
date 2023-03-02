6068b924a397422f24b848bfe66ddaf0d41f6478
Elias Maalouf <egmaalouf@gmail.com>
2023-03-02 15:33:33 +0200

    Get SSM with value

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -7,21 +7,32 @@ export class CdkStarterStack extends cdk.Stack {
 
     const emailParam = new ssm.StringParameter(this, "alerts-email-param", {
       parameterName: "/my-site/alerts-email-dev",
-      stringValue: "egmaalouf@gmail.com",
+      stringValue: "dev-email@example.com",
       description: "the email used for alerting for dev",
       type: ssm.ParameterType.STRING,
       tier: ssm.ParameterTier.STANDARD,
       allowedPattern: ".*",
     });
 
-    const environmentsParam = new ssm.StringListParameter(
-      this,
-      "environments-param",
-      {
-        parameterName: "/my-site/environments",
-        stringListValue: ["dev", "test", "prod"],
-        tier: ssm.ParameterTier.ADVANCED,
-      }
-    );
+    // const environmentsParam = new ssm.StringListParameter(
+    //   this,
+    //   "environments-param",
+    //   {
+    //     parameterName: "/my-site/environments",
+    //     stringListValue: ["dev", "test", "prod"],
+    //     tier: ssm.ParameterTier.ADVANCED,
+    //   }
+    // );
+
+    const importedParam =
+      ssm.StringParameter.fromSecureStringParameterAttributes(
+        this,
+        "imported-param-3",
+        { parameterName: "/my-app/dev/db-password", version: 1 }
+      );
+
+    new cdk.CfnOutput(this, "imported-param-3-value", {
+      value: importedParam.parameterName,
+    });
   }
 }
